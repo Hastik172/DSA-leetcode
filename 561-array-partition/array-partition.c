dc108cf3fcb6b8607b40d9d4ae538ad5dff5962a
Hastik172 <hastik2003@gmail.com>
2026-04-23 18:32:06 +0530

    Time: 19 ms (74.18%) | Memory: 11.3 MB (74.68%) - LeetSync

--- 561-array-partition/array-partition.c
+++ 561-array-partition/array-partition.c
@@ -1,20 +1,15 @@
 #include <stdlib.h>
 
-// comparator for qsort
 int cmp(const void* a, const void* b) {
     return (*(int*)a - *(int*)b);
 }
 
 int arrayPairSum(int* nums, int numsSize) {
-    // sort the array
     qsort(nums, numsSize, sizeof(int), cmp);
 
     int sum = 0;
-
-    // take every alternate element
     for (int i = 0; i < numsSize; i += 2) {
         sum += nums[i];
     }
-
     return sum;
 }
